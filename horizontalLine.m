29e8d708f52a462089300cfb05a3de9bc92d8287
dgrace6 <86323276+dgrace6@users.noreply.github.com>
2022-09-09 23:57:09 -0400

    Add files via upload

---
+++ horizontalLine.m
@@ -0,0 +1,50 @@
+function out=horizontalLine(in)
+syms teta1 teta2 TT
+teta=[teta1;teta2];
+alpha1=in(1);
+alpha2=in(2);
+L=[1,1];
+R=[L(1)/teta1,L(2)/teta2];
+R=eval(subs(R,{teta1,teta2},[alpha1,alpha2]));
+TT=eye(3);
+for i=1:2
+    T(i,:,:)=[cos(teta(i)) -sin(teta(i)) R(i)*sin(teta(i))
+    sin(teta(i)) cos(teta(i)) R(i)*(1-cos(teta(i)))
+    0 0 1];
+    TT=TT*squeeze(T(i,:,:));
+    Trans{i}=TT;
+    X(i,:)=Trans{i}*[0;0;1];
+end
+beta1=linspace(.001,alpha1,15);
+beta2=linspace(.001,alpha2,15);
+for i=1:15
+    X1(i,:)=eval(subs(X(1,:),{teta1,teta2},[beta1(i),beta2(end)]));
+    X2(i,:)=eval(subs(X(2,:),{teta1,teta2},[beta1(end),beta2(i)]));
+end
+hold on
+plot(X1(:,1),X1(:,2),'r')
+plot(X2(:,1),X2(:,2),'b')
+plot(X2(end,1),X2(end,2),'*')
+axis equal
+
+
+alpha1=linspace(.1,pi/3,10);
+alpha2=linspace(.1,pi/6,10);
+k=1;
+for i=1:length(alpha1)
+    for j=1:length(alpha1)
+        data1(k,:)=[[X2(end,1),X2(end,2)],alpha1(i)];
+        data2(k,:)=[[X2(end,1),X2(end,2)],alpha2(j)];
+        k=k+1;
+    end
+end
+
+T=1;
+dt=.05;
+t=.2:dt:T;
+x=t;
+y=ones(size(x));
+figure(1)
+plot(x,y,'k')
+end
+
---
+++ jacobiansoftrobot.m
@@ -0,0 +1,59 @@
+clc;clear all;close all;
+syms teta1 teta2 TT
+teta=[teta1;teta2];
+L=[1,1];
+R=[L(1)/teta1,L(2)/teta2];
+TT=eye(3);
+for i=1:2
+    T(i,:,:)=[cos(teta(i)) -sin(teta(i)) R(i)*sin(teta(i))
+    sin(teta(i)) cos(teta(i)) R(i)*(1-cos(teta(i)))
+    0 0 1];
+    TT=TT*squeeze(T(i,:,:));
+    Trans{i}=TT;
+    X(:,i)=Trans{i}*[0;0;1];
+end
+XX=X(1:2,2);
+J=jacobian(XX, [teta1,teta2]);
+T=1;
+dt=.05;
+t=.2:dt:T;
+x=t;
+y=ones(size(x));
+Xd=[x',y'];
+Qd=[1;1];
+for i=1:length(t)
+    E=1;
+    while E>1e-5
+        Qd=(eval(subs(J,{teta1,teta2},Qd'))^-1)*(Xd(i,:)'-eval(subs(XX, {teta1,teta2},Qd')))+Qd;
+        X1=eval(subs(XX,{teta1,teta2},Qd'));
+        E=norm(abs(Xd(i,:)'-X1));
+    end
+    Q(i,:)=Qd;
+end
+figure(2)
+plot(t,180*Q(:,1)/pi,t,180*Q(:,2)/pi)
+figure(1)
+for i=1:length(Q)
+    horizontalLine([Q(i,1),Q(i,2)])   
+end
+
+T=6;
+dt=.5;
+t=0:dt:T;
+x=.2*cos(2*pi*t/T)+1;
+y=.2*sin(2*pi*t/T)+1.2;
+Xd=[x',y'];
+Qd=[1;1];
+for i=1:length(t)
+    E=1;
+    while E>1e-5
+        Qd=(eval(subs(J,{teta1,teta2},Qd'))^-1)*(Xd(i,:)'-eval(subs(XX, {teta1,teta2},Qd')))+Qd;
+        X1=eval(subs(XX,{teta1,teta2},Qd'));
+        E=norm(abs(Xd(i,:)'-X1));
+    end
+    Q(i,:)=Qd;
+  
+end
+for i=1:length(Q)
+    elipse([Q(i,1),Q(i,2)])   
+end
